c5f67d65af3a743fc03b89049512f09e674ebcdd
myyusuf <myyusuf1911@gmail.com>
2017-06-22 13:29:03 +0700

    Fix suggestion

--- public/scripts/tweetyourfood.js
+++ public/scripts/tweetyourfood.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
   $('#btnRecipe').on('click', function() {
     console.log('recipe....');
     var tweetText = $('#txtTwitter');
+    var ingredientLinesDom = $('#ingredientLines');
     var food = tweetText.val();
     var foodArray = food.match(/:\w+/g);
     var foodToSend = foodArray[0].replace(':', '');
@@ -11,6 +12,8 @@ $( document ).ready(function() {
     }, function( resp ) {
       // var recipes = resp[0].recipe.ingredientLines.join(',');
       var recipes = resp[0].recipe.url;
+      var ingredientLines = resp[0].recipe.ingredientLines.join(',');
+      ingredientLinesDom.val(ingredientLines);
       tweetText.val(food.replace(/:/g, '') + ' ' + recipes);
       console.log(recipes); // server response
     });
